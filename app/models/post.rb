5f239bc43466928b6cc3854c6e1ae21aa8e5c3fc
Tanner <troxs690@gmail.com>
2023-02-09 14:26:03 -0600

    commit tree 20721a13d9e968215d2eb2979fe6dce78cea3a70

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content,length: { minimum: 250 }
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |p| p.match title }
+            errors.add(:title, "must be clickbait-y")
+        end
+  end
 end
